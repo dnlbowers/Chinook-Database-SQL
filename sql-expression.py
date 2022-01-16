99c8e158746912bbd452476f5dc63939c4e569e6
dnlbowers <dnlbowers@gmail.com>
2022-01-16 00:29:25 +0000

    Create table variable and make search queries

---
+++ sql-expression.py
@@ -0,0 +1,68 @@
+from select import select
+from sqlalchemy import (
+    create_engine, Table, Column, Float, ForeignKey, Integer, String, MetaData
+)
+
+# executing the instructions form our local host "chinook" db
+db = create_engine("postgresql:///chinook")
+
+meta = MetaData(db)
+
+# create variable for "Artist" table
+artist_table = Table(
+    "Artist", meta,
+    Column("ArtistId", Integer, primary_key=True),
+    Column("Name", String)
+)
+
+# create variable for Album table
+album_table = Table(
+    "Album", meta, 
+    Column("AlbumId", Integer, primary_key=True),
+    Column("Title", String),
+    Column("ArtistId", Integer, ForeignKey("artist_table.ArtistId"))
+)
+
+# create variable for Track table
+track_table = Table(
+    "Track", meta,
+    Column("TrackId", Integer, primary_key=True),
+    Column("Name", String),
+    Column("AlbumId", Integer, ForeignKey("album_table.AlbumId")),
+    Column("MediaTypeId", Integer, primary_key=False),
+    Column("GenreId", Integer, primary_key=False),
+    Column("Composer", String),
+    Column("Milliseconds", Integer),
+    Column("Bytes", Integer),
+    Column("UnitPrice", Float)
+)
+
+# making the connection
+with db.connect() as connection:
+
+    # Query 1 - select all records from the "Artist" table
+    # select_query = artist_table.select()
+
+    # Query 2 - select only the "name" column form the "Artist" table
+    # .c.Name picks a specific column header in this case "NAme"
+    # select_query = artist_table.select().with_only_columns(
+    #     [artist_table.c.Name])
+
+    # Query 3 - Select only "Queen" form the "Artist" table
+    # select_query = artist_table.select().where(
+    # artist_table.c.Name == "Queen")
+
+    # Query 4 - Select only "ArtistId" #51 form the "Artist" table
+    # select_query = artist_table.select().where(artist_table.c.ArtistId == 51)
+
+    # Query 5 - Select only "ArtistId" #51 form the "Album" table
+    # select_query = album_table.select().where(album_table.c.ArtistId == 51)
+
+    # Query 5 - Select only "composer" "Queen" form the "Track" table
+    select_query = track_table.select().where(
+        track_table.c.Composer == "Queen")
+
+    results = connection.execute(select_query)
+    # print results
+    for result in results:
+        print(result)
